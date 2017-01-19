1c1202699b574e95f64539fe1eb6e7e2b6a42279
zhukic <zhukic@mail.ru>
2017-01-19 18:43:38 +0300

    fixed bugs with camera preview

--- app/src/main/java/com/example/rus/cameraoverlay/CameraActivity.kt
+++ app/src/main/java/com/example/rus/cameraoverlay/CameraActivity.kt
@@ -3,6 +3,7 @@ package com.example.rus.cameraoverlay
 import android.hardware.Camera
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Surface
 import android.view.Window
 import android.view.WindowManager
 import kotlinx.android.synthetic.main.activity_camera.*
@@ -16,10 +17,7 @@ class CameraActivity : AppCompatActivity() {
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_camera)
 
-        val camera = Camera.open()
-
-        camera_preview.addView(CameraPreview(this, camera), 0)
+        camera_preview.addView(CameraPreview(this), 0)
     }
-
 
 }
--- app/src/main/java/com/example/rus/cameraoverlay/CameraPreview.kt
+++ app/src/main/java/com/example/rus/cameraoverlay/CameraPreview.kt
@@ -1,6 +1,7 @@
 package com.example.rus.cameraoverlay
 
 import android.content.Context
+import android.graphics.Rect
 import android.hardware.Camera
 import android.view.Surface
 import android.view.SurfaceHolder
@@ -9,29 +10,41 @@ import android.view.SurfaceView
 /**
  * Created by RUS on 13.01.2017.
  */
-class CameraPreview(context: Context, val camera: Camera) : SurfaceView(context), SurfaceHolder.Callback {
+class CameraPreview(context: Context, var camera: Camera = Camera.open(), var previewSize: Camera.Size? = null, var isCameraOpened: Boolean = true) : SurfaceView(context), SurfaceHolder.Callback {
 
     init {
         holder.addCallback(this)
     }
 
+
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        val width = resolveSize(suggestedMinimumWidth, widthMeasureSpec)
+        val height = resolveSize(suggestedMinimumHeight, heightMeasureSpec)
+        setMeasuredDimension(width, height)
+
+        if (previewSize == null) {
+            previewSize = getOptimalPreviewSize(camera.parameters.supportedPreviewSizes, width, height)
+        }
+    }
+
     override fun surfaceCreated(holder: SurfaceHolder?) {
-        val parameters = camera.parameters
-        parameters.focusMode = Camera.Parameters.FOCUS_MODE_CONTINUOUS_PICTURE
-        camera.parameters = parameters
-
-        camera.setPreviewDisplay(holder)
-        camera.startPreview()
-
-
+        if (!isCameraOpened) {
+            camera = Camera.open()
+            isCameraOpened = true
+        }
     }
 
     override fun surfaceChanged(holder: SurfaceHolder, format: Int, width: Int, height: Int) {
-        if (holder.surface == null) {
-            return
+        val parameters = camera.parameters
+        parameters.focusMode = Camera.Parameters.FOCUS_MODE_CONTINUOUS_PICTURE
+        parameters.focusAreas = listOf(Camera.Area(Rect(-100, -100, 100, 100), 100))
+        if (previewSize != null) {
+            parameters.setPreviewSize((previewSize as Camera.Size).width, (previewSize as Camera.Size).height)
         }
+        parameters.setPictureSize(1920, 1080)
+        camera.parameters = parameters
+        val supportedPreviewSizes = parameters.supportedPreviewSizes
 
-        camera.stopPreview()
         camera.setPreviewDisplay(holder)
         camera.startPreview()
     }
@@ -39,6 +52,37 @@ class CameraPreview(context: Context, val camera: Camera) : SurfaceView(context)
     override fun surfaceDestroyed(holder: SurfaceHolder?) {
         camera.stopPreview()
         camera.release()
+        isCameraOpened = false
+    }
+
+    private fun getOptimalPreviewSize(sizes: List<Camera.Size>, w: Int, h: Int): Camera.Size? {
+        val ASPECT_TOLERANCE = 0.1
+        val targetRatio = h.toDouble() / w
+
+        var optimalSize: Camera.Size? = null
+        var minDiff = java.lang.Double.MAX_VALUE
+
+        val targetHeight = h
+
+        for (size in sizes) {
+            val ratio = size.width.toDouble() / size.height
+            if (Math.abs(ratio - targetRatio) > ASPECT_TOLERANCE) continue
+            if (Math.abs(size.height - targetHeight) < minDiff) {
+                optimalSize = size
+                minDiff = Math.abs(size.height - targetHeight).toDouble()
+            }
+        }
+
+        if (optimalSize == null) {
+            minDiff = java.lang.Double.MAX_VALUE
+            for (size in sizes) {
+                if (Math.abs(size.height - targetHeight) < minDiff) {
+                    optimalSize = size
+                    minDiff = Math.abs(size.height - targetHeight).toDouble()
+                }
+            }
+        }
+        return optimalSize
     }
 
 }
